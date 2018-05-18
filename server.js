f3560277ee4f4ea792dd7aaec28d0992463921c7
2940cristian <2940cristian@gmail.com>
2018-05-18 08:53:50 -0700

    Added react as front end

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const routes = require('./api/routes/routes');
 
 const server = express();
 const corsOptions = {
+  origin: "http://localhost:3000/",
+  optionsSuccessStatus: 200
+  
   // If you're moving onto the stretch problem you'll need to set this obj with the appropriate fields
   // ensure that your client's URL/Port can achieve a Handshake
   // then pass this object to the cors() function
