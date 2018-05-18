ab4bac33e0fb4ebfdc3913e6ede41a2c2f09c2da
2940cristian <2940cristian@gmail.com>
2018-05-18 10:07:29 -0700

    Jokes styles and additional checks implemented

--- my-app/src/Components/Jokes/Jokes.js
+++ my-app/src/Components/Jokes/Jokes.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import axios from "axios"
 import {withRouter} from "react-router-dom"
+import "../Jokes/Jokes.css"
 class Jokes extends Component {
     constructor() {
         super();
@@ -26,8 +27,14 @@ class Jokes extends Component {
                 jokes: response.data
             });
         }).catch(err => {
+            alert("Please login to view jokes")
             this.props.history.push('/login')
         })
+    }
+
+    deleteToken = () => {
+        localStorage.removeItem("token")
+        this.props.history.push('/login')
     }
 
 
@@ -36,11 +43,18 @@ class Jokes extends Component {
         return(
             <div>
                 {this.state.jokes.length < 1 ? (
-                    <h1>access denied</h1>
+                    <h1>loading</h1>
                 ) : (
-                    <ul>
-                        {this.state.jokes.map(jokes => <div key={jokes.id}><h2>{jokes.setup}</h2><h3>{jokes.punchline}</h3></div>)}
-                    </ul>
+                    <div>
+                        <div className="header">
+                            <h1> DAD JOKES!</h1>
+                            <button onClick={this.deleteToken}>Sign out</button>
+                        </div>
+
+                        <ul>
+                            {this.state.jokes.map(jokes => <div className="card" key={jokes.id}><h2>{jokes.setup}</h2><h3>{jokes.punchline}</h3></div>)}
+                        </ul>
+                    </div>
                     // <div>Hello</div>
                 )}
             </div>
